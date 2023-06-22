338e137c4b539db0762e02d2e8b14f2c1f4b1680
skonhwang <seungkon.hwang@dsrvlabs.com>
2023-06-22 17:20:30 +0900

    Add to query data from graphGL

--- go.mod
+++ go.mod
@@ -5,12 +5,15 @@ go 1.20
 require (
 	github.com/dsrvlabs/vatz-proto v0.0.0-20230303182744-af9198f0e918
 	github.com/dsrvlabs/vatz/sdk v0.0.0-20230616221656-770304d81d3e
+	github.com/machinebox/graphql v0.2.2
 	golang.org/x/net v0.11.0
 	google.golang.org/protobuf v1.30.0
 )
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matryer/is v1.4.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rs/zerolog v1.26.1 // indirect
 	golang.org/x/sys v0.9.0 // indirect
 	golang.org/x/text v0.10.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 
 	pluginpb "github.com/dsrvlabs/vatz-proto/plugin/v1"
 	"github.com/dsrvlabs/vatz/sdk"
+	"github.com/machinebox/graphql"
 	"golang.org/x/net/context"
 	"google.golang.org/protobuf/types/known/structpb"
 )
@@ -13,9 +15,9 @@ import (
 const (
 	// Default values.
 	defaultAddr = "127.0.0.1"
-	defaultPort = 9091
+	defaultPort = 9001
 
-	pluginName = "YOUR_PLUGIN_NAME"
+	pluginName = "etherfi"
 )
 
 var (
@@ -40,10 +42,51 @@ func main() {
 	}
 }
 
+func query_gql() {
+	// create a client (safe to share across requests)
+	client := graphql.NewClient("https://api.studio.thegraph.com/query/41778/etherfi-mainnet/0.0.3")
+
+	// make a request
+	req := graphql.NewRequest(`
+		query {
+				bids(where: { status: "WON", bidderAddress: "0x7C0576343975A1360CEb91238e7B7985B8d71BF4" }) {
+					id
+				}
+			}
+		`)
+	// run it and capture the response
+	var respData map[string]interface{}
+	if err := client.Run(context.Background(), req, &respData); err != nil {
+		log.Fatal(err)
+	}
+	bids, ok := respData["bids"]
+	if !ok {
+		fmt.Println("bids not found in respData")
+	} else {
+		for _, bid := range bids.([]interface{}) {
+			bidMap := bid.(map[string]interface{})
+			if idValue, ok := bidMap["id"]; ok {
+				fmt.Println("ID:", idValue)
+			} else {
+				fmt.Println("ID not found in bid")
+			}
+		}
+	}
+	/*
+		bytes, err := json.MarshalIndent(respData, "", "    ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(bytes))
+	*/
+}
+
 func pluginFeature(info, option map[string]*structpb.Value) (sdk.CallResponse, error) {
 	// TODO: Fill here.
+	//	fmt.Println("asdfasdfasdf")
+	query_gql()
 	ret := sdk.CallResponse{
-		FuncName:   "YOUR_FUNCTION_NAME",
+		FuncName:   "etherfi_func",
 		Message:    "YOUR_MESSAGE_CONTENTS",
 		Severity:   pluginpb.SEVERITY_UNKNOWN,
 		State:      pluginpb.STATE_NONE,
